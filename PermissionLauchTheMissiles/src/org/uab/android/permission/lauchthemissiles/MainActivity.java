6f266adea446bbdee0faacc69c7835ba1ebd532b
Cristian Tanas <c.tanas@gmail.com>
2014-10-30 16:04:55 +0100

    Initial commit for PermissionLaunchTheMissiles project.

---
+++ PermissionLauchTheMissiles/src/org/uab/android/permission/lauchthemissiles/MainActivity.java
@@ -0,0 +1,13 @@
+package org.uab.android.permission.lauchthemissiles;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
